f9dfa4f570ca67ee6ff32336ab347d46f671b265
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-08-29 13:47:51 +0530

    role form edit.. fixes

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Context } from "../../context";
 import UserRoutes from "../../components/routes/UserRoutes";
+import { Card } from "@mui/material";
 
 const userProfile = () => {
   const { state } = useContext(Context);
@@ -9,6 +10,7 @@ const userProfile = () => {
   return (
     <UserRoutes>
       <h2 className="text-center bg-success p-5">{JSON.stringify(user)}</h2>
+      {user && <Card></Card>}
     </UserRoutes>
   );
 };
